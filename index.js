a6763bc7eb3fd5ad9a4b984a284cfbd05daa8a01
prathap.karaka <prathap.karaka@kotak.com>
2024-04-17 01:28:32 +0530

    setup push

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/login', (req, res) => {
+  res.send('<h1>please login to continue the session</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
