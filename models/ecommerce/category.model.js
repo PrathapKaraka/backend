e02507f3d065121284e0f5485170ebaae74b8357
PrathapKaraka <venkatasivaprathap1002@gmail.com>
2024-04-24 14:46:16 +0530

    todods schema and ecommerce schema

---
+++ models/ecommerce/category.model.js
@@ -0,0 +1,14 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+
+
+},{timestamps: true})
+
+
+export const Category = mongoose.model("Category", categorySchema);
---
+++ models/ecommerce/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        isUnique: true,
+        lowercase: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        isUnique: true,
+        lowercase: true,
+    },
+    password :{
+        type : String,
+        required: [true, 'password is required'],
+        isUnique: true,
+    },
+
+},{timestamps: true})
+
+export const User = mongoose.model("User", userSchema);
